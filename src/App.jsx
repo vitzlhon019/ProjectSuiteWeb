b34916c8826981d3f5e4103a0e387093ecb1066d
Marlon <sangalangmarlon19@gmail.com>
2025-06-08 23:29:57 +0800

    Test api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('/api/User/GetAllUsers') 
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('API response was not ok')
+        }
+        
+        return res.json()
+      })
+      .then((data) => {
+        console.log(data);
+        setUsers(data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.error('Error fetching users:', err)
+        setLoading(false)
+      })
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ padding: '2rem' }}>
+      <h1>Users</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {users.map((user) => (
+            <li key={user.userId}>
+              {user.employeeName} - {user.email}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
 
